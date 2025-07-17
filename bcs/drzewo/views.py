77d40ddbefec558f875fbf3ad88508cdba776bcd
Szymon <zwiasszymon@gmail.com>
2025-07-18 00:05:13 +0200

    drzewo: fixed serve_full_tree_form_view's issue with form validation if onp was left unchecked

--- bcs/drzewo/views.py
+++ bcs/drzewo/views.py
@@ -10,24 +10,21 @@ from drzewo.utils.draw_a_tree import generate_full_tree, generate_scoped_tree
 
 @require_GET
 def serve_full_tree_form_view(request):
-    if 'only_known_parents' in request.GET:
-        form = FullTreeRenderForm(request.GET)
+    form = FullTreeRenderForm(request.GET)
 
-        if form.is_valid():
-            onp = form.cleaned_data['only_known_parents']
+    if form.is_valid():
+        onp = form.cleaned_data['only_known_parents']
 
-            title = "full_tree" + ("_onp" if onp else "")
-            path = f"/home/szymon/Desktop/bcs/bcs/drzewo/trees/{title}.png"
+        title = "full_tree" + ("_onp" if onp else "")
+        path = f"/home/szymon/Desktop/bcs/bcs/drzewo/trees/{title}.png"
 
-            if not os.path.exists(path):
-                generate_full_tree(path, onp)
+        if not os.path.exists(path):
+            generate_full_tree(path, onp)
 
-            if os.path.exists(path): # TODO: generate anyway if related models' records changed
-                return FileResponse(open(path, "rb"), content_type="image/png")
+        if os.path.exists(path): # TODO: generate anyway if related models' records changed
+            return FileResponse(open(path, "rb"), content_type="image/png")
 
-            raise Http404("Image not found after generation")
-    else:
-        form = FullTreeRenderForm()
+        raise Http404("Image not found after generation")
 
     return render(request, "drzewo/full_tree_generation.html", {"form": form})
 
