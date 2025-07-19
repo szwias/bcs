16fe14723434ea1e09dc0c5e52b6f30a8697b50c
Szymon <zwiasszymon@gmail.com>
2025-07-19 23:35:45 +0200

    czapki: added filtering by miejsca.Uczelnia to Czapka admin panel

--- bcs/czapki/admin.py
+++ bcs/czapki/admin.py
@@ -1,5 +1,30 @@
 from core.utils.automation.BaseAdmin import *
-from .models import RodzajCzapki
+from .models import RodzajCzapki, Czapka
+
+from django.contrib import admin
+from django.apps import apps
+
+class UczelniaFilter(admin.SimpleListFilter):
+    title = 'Uczelnia'
+    parameter_name = 'uczelnia'
+
+    def lookups(self, request, model_admin):
+        # Dynamically get the model
+        Uczelnia = apps.get_model('miejsca', 'Uczelnia')
+        # Get distinct uczelnie used in related Czapka entries
+        return [
+            (u.id, str(u))
+            for u in Uczelnia.objects.filter(wydzial__czapka__isnull=False).distinct()
+        ]
+
+    def queryset(self, request, queryset):
+        if self.value():
+            return queryset.filter(wydzial__uczelnia__id=self.value())
+        return queryset
+
+@admin.register(Czapka)
+class CzapkaAdmin(BaseModelAdmin):
+    list_filter = [UczelniaFilter]
 
 @admin.register(RodzajCzapki)
 class RodzajCzapkiAdmin(BaseModelAdmin):
@@ -7,6 +32,7 @@ class RodzajCzapkiAdmin(BaseModelAdmin):
 
 register_all_models(
     custom_admins={
+        Czapka: CzapkaAdmin,
         RodzajCzapki: RodzajCzapkiAdmin,
     }
 )
