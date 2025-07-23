d1d021d7f21a065e1a1a22cdfee449bc1f8b8916
Szymon <zwiasszymon@gmail.com>
2025-07-23 21:14:05 +0200

    osoby: used UsedContentTypeFilter in OsobaAdmin.list_filter

--- bcs/osoby/admin.py
+++ bcs/osoby/admin.py
@@ -1,4 +1,5 @@
 from core.utils.automation.BaseAdmin import *
+from core.utils.filters import UsedContentTypeFilter
 from .models import (
     Bean,
     Czlonek,
@@ -13,33 +14,15 @@ from .models import (
 )
 from kronika.inlines import CharakterystykaDzialanZarzaduInline
 
-from django.contrib.contenttypes.models import ContentType
 
-
-class UsedContentTypeFilter(admin.SimpleListFilter):
+class OsobyUsedContentTypeFilter(UsedContentTypeFilter):
     title = "Typ osoby"
     parameter_name = "typ_osoby"
-
-    def lookups(self, request, model_admin):
-        # Get only content types that are actually used in this model
-        used_ct_ids = model_admin.model.objects.values_list(
-            "polymorphic_ctype", flat=True
-        ).distinct()
-        used_cts = ContentType.objects.filter(id__in=used_ct_ids)
-        return [
-            (ct.id, apps.get_model(ct.app_label, ct.model)._meta.verbose_name)
-            for ct in used_cts
-        ]
-
-    def queryset(self, request, queryset):
-        if self.value():
-            return queryset.filter(polymorphic_ctype_id=self.value())
-        return queryset
 
 
 @admin.register(Osoba)
 class OsobaAdmin(BaseModelAdmin):
-    list_filter = [UsedContentTypeFilter]
+    list_filter = [OsobyUsedContentTypeFilter]
 
 
 @admin.register(Bean)
