b3a598366ddcd201b8064041edc57830dd80d6c0
Szymon <zwiasszymon@gmail.com>
2025-07-29 12:10:00 +0200

    refactor(multimedia.models): create Obraz polymorphic model

--- bcs/multimedia/models.py
+++ bcs/multimedia/models.py
@@ -1,3 +1,20 @@
+import os
+
 from django.db import models
+from django.utils import timezone
+from polymorphic.models import PolymorphicModel
+
+from core.utils.Consts import MEDIUM_LENGTH
+
+
+class Obraz(PolymorphicModel):
+    tytul = models.CharField(max_length=MEDIUM_LENGTH, verbose_name="Tytuł")
+
+    data = models.DateField(
+        default=timezone.now, blank=True, verbose_name="Data wykonania"
+    )
+
+    opis = models.TextField(blank=True, verbose_name="Opis")
+
 
 # Create your models here.
