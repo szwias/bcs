4b316bf0f9828c6f7c2eb7f4d582ad7d12feb251
Szymon <zwiasszymon@gmail.com>
2025-08-21 16:24:27 +0200

    refactor(kalendarz): introduce WydarzenieKalendarzowe base and WydarzenieDummy subclass

--- bcs/kalendarz/models.py
+++ bcs/kalendarz/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from django.utils import timezone
+from polymorphic.models import PolymorphicModel
 
 from core.utils.Consts import MAX_LENGTH, MEDIUM_LENGTH
 
@@ -105,6 +106,29 @@ class TypWyjazdu(models.Model):
         return TypWyjazdu.objects.get(typ="Nie dotyczy")
 
 
+class WydarzenieKalendarzowe(PolymorphicModel):
+    
+    nazwa = models.CharField(max_length=MAX_LENGTH, verbose_name="Nazwa")
+
+    data_rozpoczecia = models.DateField(
+        default=timezone.now(), verbose_name="Data rozpoczecia"
+    )
+
+    link = models.URLField(blank=True, verbose_name="Link do wydarzenia na FB")
+
+    opis = models.TextField(blank=True, verbose_name="Opis")
+
+    class Meta:
+        verbose_name = "Wydarzenie kalendarzowe"
+        verbose_name_plural = "Wydarzenia kalendarzowe"
+        ordering = ["-data_rozpoczecia"]
+
+    def __str__(self):
+        return f"{self.data_rozpoczecia} - {self.nazwa}"
+
+    pass
+
+
 class Wydarzenie(models.Model):
 
     nazwa = models.CharField(max_length=MAX_LENGTH, verbose_name="Nazwa")
@@ -180,4 +204,67 @@ class Wydarzenie(models.Model):
         name += f': {typ} "{self.nazwa}"'
         return name
 
+class WydarzenieDummy(WydarzenieKalendarzowe):
 
+    czy_jednodniowe = models.BooleanField(
+        default=True, verbose_name="Jednodniowe"
+    )
+
+    data_zakonczenia = models.DateField(
+        default=timezone.now, verbose_name="Data zakończenia"
+    )
+
+    miejsca = models.ManyToManyField(
+        "miejsca.Miejsce",
+        blank=True,
+        verbose_name="Miejsca",
+    )
+
+    czy_to_wyjazd = models.BooleanField(
+        default=False, verbose_name="Czy to wyjazd?"
+    )
+
+    typ_wydarzenia = models.ForeignKey(
+        TypWydarzenia,
+        blank=True,
+        null=True,
+        on_delete=models.SET_NULL,
+        verbose_name="Typ wydarzenia",
+    )
+
+    typ_wyjazdu = models.ForeignKey(
+        TypWyjazdu,
+        blank=True,
+        null=True,
+        on_delete=models.SET_NULL,
+        verbose_name="Typ wyjazdu",
+    )
+
+    uczestnicy = models.ManyToManyField(
+        "osoby.Osoba",
+        blank=True,
+        verbose_name="Uczestnicy wydarzenia",
+    )
+
+    class Meta:
+        verbose_name = "Wydarzenie"
+        verbose_name_plural = "Wydarzenia"
+        ordering = ["-data_rozpoczecia"]
+
+    def __str__(self):
+        name = f"{self.data_rozpoczecia}"
+        if self.data_zakonczenia != self.data_rozpoczecia:
+            name += f" - {self.data_zakonczenia}"
+
+        if self.typ_wydarzenia and not self.typ_wydarzenia.is_sentinel():
+            typ = str(self.typ_wydarzenia)
+        elif self.typ_wyjazdu:
+            typ = str(self.typ_wyjazdu)
+        else:
+            typ = ""
+
+        if typ.lower() in self.nazwa.lower():
+            typ = ""
+
+        name += f': {typ} "{self.nazwa}"'
+        return name
