e5c9b68b247dad3fd8db7f05e6c301a355ef07ac
Szymon <zwiasszymon@gmail.com>
2025-08-25 21:49:40 +0200

    feat(count.sh): add JSON files tracking

--- bcs/scripts/count.sh
+++ bcs/scripts/count.sh
@@ -2,17 +2,30 @@
 # Script to count lines in different file types excluding certain directories
 
 # Count lines in Python files excluding migrations
-python_lines=$(find . -name '*.py' ! -path '*/migrations/*' -exec cat {} + | wc -l)
+python_lines=$(find . -name '*.py' \
+    ! -path "*/scripts/other_locations*" \
+    ! -path '*/migrations/*' \
+    -exec cat {} + | wc -l)
 echo "Python lines: $python_lines"
 
 # Count lines in SQL files excluding backups
-sql_lines=$(find . -name '*.sql' ! -path '*/backups/*' -exec cat {} + | wc -l)
+sql_lines=$(find . -name '*.sql' \
+    ! -path "*/scripts/other_locations*" \
+    ! -path '*/backups/*' \
+    -exec cat {} + | wc -l)
 echo "SQL lines: $sql_lines"
 
 # Count lines in HTML files
 html_lines=$(find . -name '*.html' -exec cat {} + | wc -l)
 echo "HTML lines: $html_lines"
 
-# Count lines in shell files
-bash_lines=$(find . -name '*.sh' -exec cat {} + | wc -l)
+# Count lines in Bash files
+bash_lines=$(find . -name '*.sh' \
+    ! -path "*/scripts/other_locations*" \
+    -exec cat {} + | wc -l)
 echo "Bash lines: $bash_lines"
+
+# Count lines in JSON files
+json_lines=$(find . -name '*.json' \
+    -exec cat {} + | wc -l)
+echo "JSON lines: $json_lines"
