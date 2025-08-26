411d55e306f15c460fae9bf41573a6621bec8a0a
Szymon <zwiasszymon@gmail.com>
2025-08-26 15:34:03 +0200

    feat(settings): integrate django-cleanup for automatic file deletion
    Adds the django-cleanup package to installed apps.
    This ensures that files referenced by FileFields or ImageFields
    are automatically deleted when the related model instance is removed.

--- bcs/bcs/settings.py
+++ bcs/bcs/settings.py
@@ -55,6 +55,7 @@ INSTALLED_APPS = [
     "zrodla",
     "dal",  # django-autocomplete-light app
     "dal_select2",  # for the Select2 widget
+    "django_cleanup.apps.CleanupConfig", # cleans up files
 ]
 
 MIDDLEWARE = [
