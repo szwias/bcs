0e545d9de9e24a7d42e9d656f24551e8e2f87e19
Szymon <zwiasszymon@gmail.com>
2025-08-27 11:15:10 +0200

    refactor(kalendarz): move YearListFilter from admin.py to filters.py

--- bcs/kalendarz/admin.py
+++ bcs/kalendarz/admin.py
@@ -11,21 +11,11 @@ from kalendarz.models import (
     DepositioBeanorum,
 )
 from .inlines import ZdarzenieInline
+from .filters import YearListFilter
 from multimedia.inlines import ObrazWydarzenieInline, ObrazZdarzenieInline
 
 
-class YearListFilter(admin.SimpleListFilter):
-    title = "Rok"
-    parameter_name = "rok"
 
-    def lookups(self, request, model_admin):
-        years = range(ROK_ZALOZENIA, BIEZACY_ROK + 1)
-        return [(year, year) for year in years]
-
-    def queryset(self, request, queryset):
-        if self.value():
-            return queryset.filter(data_rozpoczecia__year=self.value())
-        return queryset
 
 
 @admin.register(DepositioBeanorum)
