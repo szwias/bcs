6897c5e9a92b42484a54c5b0068e695055b4790d
Szymon <zwiasszymon@gmail.com>
2025-08-28 16:28:13 +0200

    feat(skarbiec): add list_display and TransakcjaInline to KontoAdmin

--- bcs/skarbiec/admin.py
+++ bcs/skarbiec/admin.py
@@ -1,9 +1,26 @@
+from nested_admin.nested import NestedModelAdmin
+
 from core.utils.automation.BaseAdmin import (
     admin,
     BaseModelAdmin,
     register_all_models
 )
+from skarbiec.models import (
+    Konto,
+)
+from .inlines import TransakcjaInline
+
+
+@admin.register(Konto)
+class KontoAdmin(NestedModelAdmin, BaseModelAdmin):
+    list_display = ["wlasciciel", "opis"]
+    inlines = [TransakcjaInline]
+
+
 
 register_all_models(
     custom_admins={}
+    custom_admins={
+        Konto: KontoAdmin,
+    }
 )
