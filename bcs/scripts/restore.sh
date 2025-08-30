e8acce5dbe0786cbf76e4dd662d6655d190c0b8d
Szymon <zwiasszymon@gmail.com>
2025-08-30 17:02:46 +0200

    chore(scripts): add `restore` command for database
    To restore the database from latest backup in /baza/baza.sql type `restore` in the terminal

---
+++ bcs/scripts/restore.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+set -e # Safe exit on fail
+
+# Dropping the database
+psql -U postgres -c "DROP DATABASE IF EXISTS bcs_db"
+
+# Creating the database
+psql -U postgres -c "CREATE DATABASE bcs_db"
+
+# Restoring the database
+psql -U postgres -d bcs_db -f baza/baza.sql 2>&1
+
+echo "Database restored successfully"
