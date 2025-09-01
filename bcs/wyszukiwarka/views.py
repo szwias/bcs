ffb147d671c67c5505a5b6d375e809dedf2afb51
Szymon <zwiasszymon@gmail.com>
2025-09-01 23:09:08 +0200

    fix(wyszukiwarka): remove the limit of search results

--- bcs/wyszukiwarka/views.py
+++ bcs/wyszukiwarka/views.py
@@ -24,7 +24,7 @@ def search(request):
             qs = (
                 model.objects.annotate(rank=SearchRank(vector, search_query))
                 .filter(rank__gt=0)
-                .order_by("-rank")[:10]
+                .order_by("-rank")
             )
 
             for obj in qs:
