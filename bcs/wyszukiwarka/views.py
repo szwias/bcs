a53cba290875df8cc7c0ad2a1478b51f6b08c00f
Szymon <zwiasszymon@gmail.com>
2025-09-01 16:59:20 +0200

    feat(wyszukiwarka): add URLs of found objects to `search` view

--- bcs/wyszukiwarka/views.py
+++ bcs/wyszukiwarka/views.py
@@ -1,26 +1,23 @@
 from django.shortcuts import render
-from django.contrib.postgres.search import SearchVector, SearchQuery, SearchRank
-from kronika.models import PodsumowanieKadencji
+from django.contrib.postgres.search import SearchQuery, SearchRank, SearchVector
+from kronika.models import PodsumowanieKadencji as P
 
 def search(request):
     q = request.GET.get("q", "")
     results = []
-
-    if query:
-        search_vector = SearchVector("podsumowanie")
-        search_query = SearchQuery(query)
-
-        results = (
-            # PodsumowanieKadencji.objects
-            # .annotate(rank=SearchRank(search_vector, search_query))
-            # .filter(rank__gte=0.1)  # optional: filter out very low matches
-            # .order_by("-rank")
-            PodsumowanieKadencji.objects.annotate(
-                vector=search_vector
-            ).filter(vector=search_query)
+    if q:
+        query = SearchQuery(q)
+        results_qs = (
+            P.objects.annotate(rank=SearchRank(SearchVector("podsumowanie"), query))
+            .filter(podsumowanie__search=q)
+            .order_by("-rank")
         )
 
-    return render(request, "wyszukiwarka/search_results.html", {
-        "query": query,
-        "results": results,
-    })
+        for obj in results_qs:
+            results.append({
+                "title": str(obj),
+                "snippet": obj.podsumowanie[:150],
+                "admin_url": f"/admin/{obj._meta.app_label}/{obj._meta.model_name}/{obj.pk}/change/"
+            })
+
+    return render(request, "wyszukiwarka/search_results.html", {"results": results, "query": q})
