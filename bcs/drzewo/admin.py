dc4676920cc979aa30a486fae878965383cdcd93
Szymon <zwiasszymon@gmail.com>
2025-09-02 14:09:03 +0200

    refactor(drzewo): remove unnecessary files from `drzewo` app

--- bcs/drzewo/admin.py
+++
@@ -1,7 +0,0 @@
-from core.utils.automation.BaseAdmin import (
-    admin,
-    BaseModelAdmin,
-    register_all_models,
-)
-
-register_all_models()
--- bcs/drzewo/autocomplete_urls.py
+++
@@ -1,5 +0,0 @@
-# from .views import autocomplete_urls
-
-app_name = "drzewo_autocomplete"
-
-# urlpatterns = autocomplete_urls
