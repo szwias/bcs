d043a297a197b8eb205c92071eb458dc1eb9a1c8
Szymon <zwiasszymon@gmail.com>
2025-09-05 23:02:09 +0200

    refactor(wyszukiwarka): simplify `registry.py` logic
    - SEARCH_REGISTRY is now a list of models instead of dictionary
    - remove `register_model()` and `get_registered_models()` functions

--- bcs/wyszukiwarka/registry.py
+++ bcs/wyszukiwarka/registry.py
@@ -1,44 +1,15 @@
 from django.apps import apps
 
-SEARCH_REGISTRY = {}
+SEARCH_REGISTRY = []
 
 
-def register_model(model, search_fields, title_field=None, snippet_func=None):
-    """
-    Register a model for wyszukiwarka.
-    - search_fields: list of fields to search
-    - title_field: field to use as title
-    - snippet_func: function(instance) -> string for snippet
-    """
-    SEARCH_REGISTRY[model] = {
-        "search_fields": search_fields,
-        "title_field": title_field,
-        "snippet_func": snippet_func,
-    }
-
-
-def get_registered_models():
-    return SEARCH_REGISTRY.keys()
-
-
-def register_search(apps_to_register, snippet_attr="snippet"):
+def register_search(apps_to_register):
     """
     Automatically register all models in the given apps.
     - apps_to_register: list of app names (strings)
-    - snippet_attr: the method to call on model instances for snippet
     """
-
     for app_label in apps_to_register:
         app_config = apps.get_app_config(app_label)
         for model in app_config.get_models():
-            # Only register models that actually have the snippet method
-            snippet_func = getattr(model, snippet_attr, None)
-            if snippet_func is None:
-                pass
-
-            register_model(
-                model,
-                search_fields=[f.name for f in model._meta.get_fields()],
-                title_field=None,  # use __str__() by default
-                snippet_func=snippet_func,
-            )
+            if getattr(model, "snippet", None):
+                SEARCH_REGISTRY.append(model)
