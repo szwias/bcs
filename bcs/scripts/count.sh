9bab3f993d717be083266934357cc7c3fc632a17
Szymon <zwiasszymon@gmail.com>
2025-09-06 17:56:58 +0200

    chore(scripts): add css lines tracking to `count` script

--- bcs/scripts/count.sh
+++ bcs/scripts/count.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
 # Script to count lines in different file types excluding certain directories
 
+echo
+echo "BACKEND:"
+
 # Count lines in Python files excluding migrations
 python_lines=$(find . -name '*.py' \
     ! -path "*/scripts/other_locations*" \
@@ -15,17 +18,31 @@ sql_lines=$(find . -name '*.sql' \
     -exec cat {} + | wc -l)
 echo "SQL lines: $sql_lines"
 
-# Count lines in HTML files
-html_lines=$(find . -name '*.html' -exec cat {} + | wc -l)
-echo "HTML lines: $html_lines"
-
 # Count lines in Bash files
 bash_lines=$(find . -name '*.sh' \
     ! -path "*/scripts/other_locations*" \
     -exec cat {} + | wc -l)
 echo "Bash lines: $bash_lines"
 
+echo
+echo "FRONTEND:"
+
+# Count lines in HTML files
+html_lines=$(find . -name '*.html' -exec cat {} + | wc -l)
+echo "HTML lines: $html_lines"
+
+# Count lines in CSS files
+css_lines=$(find . -name '*.css' \
+   ! -path "*/staticfiles/*" \
+   -exec cat {} + | wc -l)
+echo "CSS lines: $css_lines"
+
+echo
+echo "MEDIA:"
+
 # Count lines in JSON files
 json_lines=$(find . -name '*.json' \
     -exec cat {} + | wc -l)
 echo "JSON lines: $json_lines"
+
+echo
