83728c87c3477ad251b997910ac3a6adaa998660
Szymon <zwiasszymon@gmail.com>
2025-09-15 19:09:44 +0200

    chore(scripts): change behavior and usage of `add` command
    Format all shell scripts from bcs/scripts/code_repr, copy them to .venv/bin/ and make executable; create symlinks to those executables in bcs/scripts/
    
    No longer takes arguments and does not copy code from .venv/bin/* to code_repr/*

--- bcs/scripts/code_repr/add.bash
+++ bcs/scripts/code_repr/add.bash
@@ -2,40 +2,43 @@
 # -----------------------------------------------------------------------------
 # Script: add
 # Description:
-#   If a CLI command with the same name already exists in the virtualenv bin,
-#   back it up to scripts/code_repr/<name>.sh.
-#   Otherwise, copy the new script to the virtualenv bin, make it executable,
-#   and create a symlink in scripts/.
+#   Format all shell scripts from bcs/scripts/code_repr, copy them to .venv/bin/
+#   and make executable; create symlinks to those executables in bcs/scripts/
 #
-# Usage:
-#   add command_name
+# Usage: add
 # -----------------------------------------------------------------------------
 
+set -euo pipefail
+
 repo_root=$(git rev-parse --show-toplevel)
-cd "$repo_root/bcs/" || exit 1
+cd "$repo_root/" || exit 1
 
-venv_bin="../.venv/bin"
-symlink_dir="scripts"
-backup_dir="scripts/code_repr"
-new_name="$1"
-file="$backup_dir/$1.bash"
+venv_bin=".venv/bin"
+symlink_dir="bcs/scripts"
+scripts_dir="bcs/scripts/code_repr"
 
-# Check if the command already exists
-if [[ -f "$venv_bin/$new_name" ]]; then
-    echo "Command '$new_name' already exists in $venv_bin"
-    mkdir -p "$backup_dir"
-    cp "$venv_bin/$new_name" "$backup_dir/$new_name.bash"
-    echo "Backed up existing command to $backup_dir/$new_name.bash"
-else
-    # Copy file to venv bin
-    cp "$file" "$venv_bin/$new_name"
+mkdir -p "$venv_bin" "$symlink_dir"
 
-    # Make executable
-    chmod +x "$venv_bin/$new_name"
-    echo "Created CLI command: $new_name"
+shopt -s nullglob
+for file in "$scripts_dir"/*; do
+  [ -f "$file" ] || continue
 
-    # Create symlink
-    mkdir -p "$symlink_dir"
-    ln -sf "$(realpath "$venv_bin/$new_name")" "$symlink_dir/$new_name"
-    echo "Created symlink to new command"
-fi
+  # Format file
+  shfmt -w -i 2 -s "$file"
+
+  # Extract its name
+  basename="${file##*/}"
+  name="${basename%%.*}"
+
+  # Copy the file to venv bin
+  cp "$file" "$venv_bin/$name"
+
+  # Make it executable
+  chmod +x "$venv_bin/$name"
+  echo "✅ Created CLI command: $name"
+
+  # Create symlink
+  ln -sf "$(realpath "$venv_bin/$name")" "$symlink_dir/$name"
+  echo "🔗 Created symlink: $symlink_dir/$name"
+done
+shopt -u nullglob
