223417d4c2b85983911e82d587297f6c3625ed49
Szymon <zwiasszymon@gmail.com>
2025-09-15 19:13:51 +0200

    chore(scripts): add SCSS and Bash formatting to 'pre'

--- bcs/scripts/code_repr/pre.bash
+++ bcs/scripts/code_repr/pre.bash
@@ -3,16 +3,21 @@ repo_root=$(git rev-parse --show-toplevel)
 cd "$repo_root" || exit 1
 
 # JS/CSS/JSON
-git diff --name-only -z \
-  | grep -zE '\.(js|css|json)$' \
-  | xargs -0 --no-run-if-empty prettier --write --list-different
+git diff --name-only -z |
+  grep -zE '\.(js|css|scss|json)$' |
+  xargs -0 --no-run-if-empty prettier --write --list-different
 
 # HTML
-git diff --name-only -z \
-  | grep -zE '\.html$' \
-  | xargs -0 --no-run-if-empty djlint --reformat
+git diff --name-only -z |
+  grep -zE '\.html$' |
+  xargs -0 --no-run-if-empty djlint --reformat
 
 # Python
-git diff --name-only -z \
-  | grep -zE '\.py$' \
-  | xargs -0 --no-run-if-empty black -l 79
+git diff --name-only -z |
+  grep -zE '\.py$' |
+  xargs -0 --no-run-if-empty black -l 79
+
+# Bash
+git diff --name-only -z |
+  grep -zE '\.bash$' |
+  xargs -0 --no-run-if-empty shfmt -w -i 2
