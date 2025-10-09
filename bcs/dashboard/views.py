0c96554fcbe1c112a01016d12bb2663be443acb1
Szymon <zwiasszymon@gmail.com>
2025-10-09 12:55:50 +0200

    feat(dashboard): add dashboard view
    - dashboard_view fetches all App instances
    - Renders dashboard/dashboard.html with apps context

--- bcs/dashboard/views.py
+++ bcs/dashboard/views.py
@@ -1,3 +1,12 @@
+# dashboard/views.py
 from django.shortcuts import render
+from .models import App
 
-# Create your views here.
+
+def dashboard_view(request):
+    apps = App.objects.all()
+    return render(
+        request=request,
+        template_name="dashboard/dashboard.html",
+        context={"apps": apps},
+    )
