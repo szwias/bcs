c1600b83852b7ccd6f7d445a5b3ee16c6104d3c5
Szymon <zwiasszymon@gmail.com>
2025-11-11 10:08:09 +0100

    fix(drzewo/utils): skip empty layer groups when modifying structure
    Prevented addition of empty year-layer keys in `modify_layers_structure`
    to improve graph layout integrity and avoid unwanted layer-merging in Graphviz.
    
    Affects: bcs/drzewo/utils/essentials.py

--- bcs/drzewo/utils/essentials.py
+++ bcs/drzewo/utils/essentials.py
@@ -7,7 +7,8 @@ def modify_layers_structure(layers):
     new_layers = defaultdict(list)
     for year in layers:
         for layer, contents in enumerate(layers[year]):
-            new_layers[f"{year}_{layer}"].extend(contents)
+            if len(contents):
+                new_layers[f"{year}_{layer}"].extend(contents)
     return new_layers
 
 
