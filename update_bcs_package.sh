b1674ee4637f8f32a58824688fb6108c65dee842
Szymon <zwiasszymon@gmail.com>
2026-01-22 15:31:42 +0100

    refactor(bcs_package): simplify the update_bcs_package.sh
    Copy database dump straight to bcs_package/src/ instead, without intermediate locations.
    
    Affected files:
    - update_bcs_package.sh

--- update_bcs_package.sh
+++ update_bcs_package.sh
@@ -1,9 +1,6 @@
 #!/bin/bash
 backup
 cd /home/szymon/Desktop/bcs/ || exit
-cp bcs/baza/baza_*.sql bcs_dump.sql
-echo "✅ Current database dump copied"
-sed -i 's/\bpostgres\b/projectuser/g' bcs_dump.sql
-echo "✅ Changed dump's user to 'projectuser'"
-cp bcs_dump.sql bcs_package/src/bcs_dump.sql
-echo "✅ Database copied to 'bcs_package'"
+cp bcs/baza/baza_*.sql bcs_package/src/bcs_dump.sql
+sed -i 's/\bpostgres\b/projectuser/g' bcs_package/src/bcs_dump.sql
+echo "✅ Package updated"
